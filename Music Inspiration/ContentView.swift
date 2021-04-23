04d3bc218d82b8e50ec400ebdb08c7a7be91da48
mgannon3500 <mgannon3500@vt.edu>
2021-04-23 18:58:34 -0400

    Initial Commit

---
+++ Music Inspiration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Inspiration
+//
+//  Created by Michael Gannon on 4/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
